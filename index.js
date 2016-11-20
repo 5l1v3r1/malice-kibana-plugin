44f2a8febbae5e61292aecaa661866b9dc03d64e
blacktop <blacktop@users.noreply.github.com>
2016-11-20 15:59:34 -0700

    Initialize Kibana Plugin (generator-kibana-plugin v5.0.1)

---
+++ index.js
@@ -0,0 +1,36 @@
+import exampleRoute from './server/routes/example';
+
+export default function (kibana) {
+  return new kibana.Plugin({
+    require: ['elasticsearch'],
+
+    uiExports: {
+      
+      app: {
+        title: 'Malice',
+        description: 'Malice Kibana Plugin',
+        main: 'plugins/malice/app'
+      },
+      
+      
+      hacks: [
+        'plugins/malice/hack'
+      ]
+      
+    },
+
+    config(Joi) {
+      return Joi.object({
+        enabled: Joi.boolean().default(true),
+      }).default();
+    },
+
+    
+    init(server, options) {
+      // Add server routes and initalize the plugin here
+      exampleRoute(server);
+    }
+    
+
+  });
+};
---
+++ public/app.js
@@ -0,0 +1,35 @@
+import moment from 'moment';
+import chrome from 'ui/chrome';
+import uiModules from 'ui/modules';
+import uiRoutes from 'ui/routes';
+
+import 'ui/autoload/styles';
+import './less/main.less';
+import template from './templates/index.html';
+
+uiRoutes.enable();
+uiRoutes
+.when('/', {
+  template,
+  resolve: {
+    currentTime($http) {
+      return $http.get('../api/malice/example').then(function (resp) {
+        return resp.data.time;
+      });
+    }
+  }
+});
+
+uiModules
+.get('app/malice', [])
+.controller('maliceHelloWorld', function ($scope, $route, $interval) {
+  $scope.title = 'Malice';
+  $scope.description = 'Malice Kibana Plugin';
+
+  var currentTime = moment($route.current.locals.currentTime);
+  $scope.currentTime = currentTime.format('HH:mm:ss');
+  var unsubscribe = $interval(function () {
+    $scope.currentTime = currentTime.add(1, 'second').format('HH:mm:ss');
+  }, 1000);
+  $scope.$watch('$destroy', unsubscribe);
+});
