b15a639e5523ab5a2c0b1fb62f6594109f96b616
blacktop <blacktop@users.noreply.github.com>
2016-11-24 17:17:18 -0700

    updates and thangs

--- public/app.js
+++ public/app.js
@@ -9,27 +9,37 @@ import template from './templates/index.html';
 
 uiRoutes.enable();
 uiRoutes
-.when('/', {
-  template,
-  resolve: {
-    currentTime($http) {
-      return $http.get('../api/malice/example').then(function (resp) {
-        return resp.data.time;
-      });
-    }
-  }
-});
+    .when('/', {
+        template,
+        resolve: {
+            currentTime($http) {
+                return $http.get('../api/malice/example').then(function(resp) {
+                    return resp.data.time;
+                });
+            }
+        }
+    });
 
 uiModules
-.get('app/malice', [])
-.controller('maliceHelloWorld', function ($scope, $route, $interval) {
-  $scope.title = 'Malice';
-  $scope.description = 'Malice Kibana Plugin';
+    .get('app/malice', [])
+    .controller('maliceHelloWorld', function($scope, $route, $interval) {
+        $scope.title = 'Malice';
+        $scope.description = 'Malice Kibana plugin';
 
-  var currentTime = moment($route.current.locals.currentTime);
-  $scope.currentTime = currentTime.format('HH:mm:ss');
-  var unsubscribe = $interval(function () {
-    $scope.currentTime = currentTime.add(1, 'second').format('HH:mm:ss');
-  }, 1000);
-  $scope.$watch('$destroy', unsubscribe);
-});
+        var currentTime = moment($route.current.locals.currentTime);
+        $scope.currentTime = currentTime.format('HH:mm:ss');
+        var unsubscribe = $interval(function() {
+            $scope.currentTime = currentTime.add(1, 'second').format('HH:mm:ss');
+        }, 1000);
+        $scope.$watch('$destroy', unsubscribe);
+    });
+
+uiModules.get('kibana', [])
+    .config(function() {
+        let config = chrome.getInjected('brandConfig', {});
+        chrome
+            .setBrand({
+                'logo': 'url(' + "public/images/malice small.png" + ') left no-repeat',
+                'smallLogo': 'url(' + "public/images/malice small.png" + ') left no-repeat'
+            })
+    });
