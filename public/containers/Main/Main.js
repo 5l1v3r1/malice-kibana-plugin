1cb076a1c6acb11b0acb94826aab799007802621
blacktop <blacktop@users.noreply.github.com>
2019-01-01 13:56:07 -0500

    tables !

--- public/containers/Main/Main.js
+++ public/containers/Main/Main.js
@@ -9,7 +9,9 @@ import {
   EuiPageContentBody,
   EuiImage,
   EuiText,
-  EuiSpacer
+  EuiSpacer,
+  EuiPanel,
+  EuiBasicTable
 } from '@elastic/eui';
 import '@elastic/eui/dist/eui_theme_dark.css';
 import rison from 'rison-node';
@@ -88,17 +90,67 @@ export class Main extends React.Component {
   };
 
   renderData() {
-    const hits = this.state.hits.map(hit => {
-      console.log('hit.file.name', hit._source.file.name);
-      return (
-        <div key={hit._id}>
-          <h2>Name: {hit._source.file.name}</h2>
-          <h4>Sha256: {hit._source.file.sha256}</h4>
-          <h3>AV: {hit._source.plugins.av.clamav.result}</h3>
-        </div>
-      );
-    });
-    return <div className="hit-list">{hits}</div>;
+    const columns = [
+      {
+        field: '_source.file.name',
+        name: 'Name',
+        sortable: true,
+        hideForMobile: true,
+        'data-test-subj': 'firstNameCell'
+      },
+      {
+        field: '_source.file.sha256',
+        name: 'sha256',
+        truncateText: true,
+        sortable: true,
+        hideForMobile: true,
+        'data-test-subj': 'firstNameCell'
+      },
+      {
+        field: '_source.plugins.av.clamav.result',
+        name: 'AV',
+        truncateText: true,
+        hideForMobile: true,
+        'data-test-subj': 'firstNameCell'
+      }
+    ];
+
+    const getRowProps = item => {
+      const { id } = item;
+      return {
+        'data-test-subj': `row-${id}`,
+        className: 'customRowClass',
+        onClick: () => console.log(`Clicked row ${id}`)
+      };
+    };
+
+    const getCellProps = (item, column) => {
+      const { id } = item;
+      const { field } = column;
+      return {
+        className: 'customCellClass',
+        'data-test-subj': `cell-${id}-${field}`,
+        textOnly: true
+      };
+    };
+
+    return <EuiBasicTable items={this.state.hits} columns={columns} rowProps={getRowProps} cellProps={getCellProps} />;
+
+    // const hits = this.state.hits.map(hit => {
+    //   console.log('hit.file.name', hit._source.file.name);
+    //   return (
+    //     <div key={hit._id} className="euiFlexItem">
+    //       <EuiPanel paddingSize="s" onClick={() => window.alert('Panel clicked')}>
+    //         <EuiText grow={false} size="s">
+    //           <h2>Name: {hit._source.file.name}</h2>
+    //           <p>Sha256: {hit._source.file.sha256}</p>
+    //           <p> AV: {hit._source.plugins.av.clamav.result}</p>
+    //         </EuiText>
+    //       </EuiPanel>
+    //     </div>
+    //   );
+    // });
+    // return <div className="hit-list">{hits}</div>;
   }
 
   render() {
